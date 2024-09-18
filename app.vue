d5969c5b191403e9bf349426e1564f510593eea1
Ezira Ashenafi <eazash22@gmail.com>
2024-09-18 12:29:27 +0300

    feat: create working  baseline

--- app.vue
+++ app.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <a-space direction="vertical">
+    <a-time-picker v-model:value="value" />
+    <a-time-picker v-model:value="strValue" value-format="HH:mm:ss" />
+  </a-space>
 </template>
+<script lang="ts" setup>
+import dayjs, { Dayjs } from 'dayjs';
+import { ref, watch } from 'vue';
+const value = ref<Dayjs>(dayjs('08:00:00', 'HH:mm:ss'));
+const strValue = ref<string>('09:00:00');
+
+watch(value, () => {
+  console.log(value.value);
+});
+watch(strValue, () => {
+  console.log(strValue.value);
+});
+</script>
